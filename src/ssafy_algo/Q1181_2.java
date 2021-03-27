1e94f6122e80bcb8743d854eb64fc841ce2f0351
zsoozsoo <zsoozsoo@naver.com>
2021-03-27 11:34:45 +0900

    Create Q1181_2.java

---
+++ src/ssafy_algo/Q1181_2.java
@@ -0,0 +1,35 @@
+package ssafy_algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Q1181_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr,new Comparator<String>(){
+
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length()==o2.length()) return o1.compareTo(o2);
+				else if(o1.length()>o2.length()) return 1;
+				else return -1;
+			}
+			
+		});
+		
+		System.out.println(arr[0]);
+		for (int i = 1; i < n; i++) {
+			if(!arr[i].equals(arr[i-1])) System.out.println(arr[i]);
+		}
+	}
+}
