9ac629e43e3e9d0e0b4e29a5f5c1167215cb6870
zsoozsoo <zsoozsoo@naver.com>
2021-04-01 04:22:04 +0900

    Create Q15657.java

---
+++ src/ssafy_algo/Q15657.java
@@ -0,0 +1,45 @@
+package ssafy_algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q15657 {
+
+	static int N,M;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		numbers = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		combi(arr,0,0);
+		System.out.println(sb);
+	}
+
+	private static void combi(int[] arr, int idx, int start) {
+		if(idx==M) {
+			for(int i=0; i<M; i++) sb.append(numbers[i]).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < arr.length; i++) {
+			numbers[idx] = arr[i];
+			combi(arr,idx+1,i);
+		}
+		
+	}
+}
