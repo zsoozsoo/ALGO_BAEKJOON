6e89ce016e9f449041c838d7d9d7e9a4dfd748fd
zsoozsoo <zsoozsoo@naver.com>
2021-04-14 05:57:30 +0900

    Update Q2564.java

--- src/ssafy_algo/Q2564.java
+++ src/ssafy_algo/Q2564.java
@@ -6,42 +6,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Q2564 {
-
+	
+	static int w,h;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int w = Integer.parseInt(st.nextToken());
-		int h = Integer.parseInt(st.nextToken());
-		int n = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken()); //가로
+		h = Integer.parseInt(st.nextToken()); //세로
+		int fullSize = 2*w + 2*h;// 사각형 전체 길이
+		int n = Integer.parseInt(br.readLine()); //상점의 갯수
+		int[] arr = new int[n]; // 사각형의 길이 넣어줄 배열
 		
-		int[][] map = new int[n+1][2];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			map[i][0] = Integer.parseInt(st.nextToken());
-			map[i][1] = Integer.parseInt(st.nextToken());
+			int dir = Integer.parseInt(st.nextToken());
+			int dis = Integer.parseInt(st.nextToken());
+			arr[i] = checkLoc(dir, dis);
 		}
 		
+		int sum = 0;
 		st = new StringTokenizer(br.readLine());
-		int dir = Integer.parseInt(st.nextToken());
-		int spot = Integer.parseInt(st.nextToken());
-		map[n][0] = dir; map[n][1] = spot;
-		
+		int store = checkLoc(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())); //상점의 사각형 길이
 		for (int i = 0; i < n; i++) {
-			int a=0, b=0;
-			
-			if((map[i][0]-dir+4)%4==1) {
-				if(dir %2 != 0) {
-					a = w+map[i][1]+spot;
-					b = w+(h-spot)+(h-map[i][1]);
-				}else {
-					a = h+map[i][1]+spot;
-					b = h+(w-spot)+(w-map[i][1]);
-				}
-			} else {
-				if()
-			}
+			int diff = Math.abs(store-arr[i]);//상점과 동근이의 차이 (시계방향으로)
+			sum += Math.min(diff, fullSize-diff); //전체사각형의 길이에서 diff빼준거랑 둘 중에 최소 구해서 sum에 더해줌
 		}
+		
+		System.out.println(sum);
+		
+	}
+	
+	private static int checkLoc(int dir,int dis) { //사각형의 길이 구해주기 (시계방향으로)
+		int res = 0;
+		switch(dir) {
+		case 1: res=dis; break;
+		case 2: res=(2*w)+h-dis; break;
+		case 3: res=(2*w)+(2*h)-dis; break;
+		case 4:	res=w+dis; break;
+		}
+		return res;
 		
 	}
 }
