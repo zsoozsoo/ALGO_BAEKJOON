8e747fbd678b9027d4405a65ce2b832516763392
zsoozsoo <zsoozsoo@naver.com>
2021-05-13 04:03:36 +0900

    Update Q1992.java

--- src/ssafy_algo/Q1992.java
+++ src/ssafy_algo/Q1992.java
@@ -7,49 +7,51 @@ import java.util.StringTokenizer;
 
 public class Q1992 {
 	
-	public static int N;
-	public static int[][] arr;
-//	public static int[] dx = {0,0,1,1};
-//	public static int[] dy = {0,1,0,1};
-	
-	public static void codeQuery(int row, int col) {
-		
-		N/=2;
-		boolean flg = false;
-		StringBuilder sb = new StringBuilder();
-		for (int i = row; i < row+N; i++) {
-			for (int j = col; j < col+N; j++) {
-					if(arr[row][col]==1) {
-						flg = true;
-					}else{
-						flg = false;
-					}
-					
-			}
-		}
-		
-		N /= 2;
-		codeQuery(row,col);
-		codeQuery(row,col+(N/2));
-		codeQuery(row+(N/2),col);
-		codeQuery(row+(N/2),col+(N/2));
-	}
+	static StringBuilder sb = new StringBuilder();
+	static int[][] arr;
 	
 	public static void main(String[] args) throws IOException {
 		
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	N = Integer.parseInt(br.readLine());
+	int n = Integer.parseInt(br.readLine());
 	
-	arr = new int[N][N];
-	for (int i = 0; i < N; i++) {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int j = 0; j < N; j++) {
-			arr[i][j] = Integer.parseInt(st.nextToken());
+	arr = new int[n][n];
+	
+	for (int i = 0; i < n; i++) {
+		String str = br.readLine();
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = str.charAt(j)-'0';
 		}
 	}
-	
-	codeQuery(0,0);
-	
 		
+	quardTree(0,0,n);	
+	System.out.println(sb.toString());
+	}
+
+	private static void quardTree(int row, int col, int size) {
+		
+		boolean check = false;
+		int idx = arr[row][col];
+		
+		for (int i = row; i < row+size; i++) {
+			for (int j = col; j < col+size; j++) {
+				if(arr[i][j]!= idx) {
+					check = true;
+					break;
+				}
+			}
+		}
+		
+		if(!check) {
+			sb.append(idx);
+			return;
+		}
+		sb.append("(");
+		size=size/2;
+		quardTree(row,col,size);
+		quardTree(row,col+size,size);
+		quardTree(row+size,col,size);
+		quardTree(row+size,col+size,size);
+		sb.append(")");
 	}
 }
