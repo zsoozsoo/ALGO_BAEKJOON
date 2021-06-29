862765f7c3ffb27926051dff2657d54de0f7e88e
zsoozsoo <zsoozsoo@naver.com>
2021-06-29 16:12:12 +0900

    Q2567

--- src/ssafy_algo/Q2567.java
+++ src/ssafy_algo/Q2567.java
@@ -12,22 +12,33 @@ public class Q2567 {
 	 int n = Integer.parseInt(br.readLine());
 	 
 	 int[][] arr = new int[101][101];
+	 int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+	 
 	 for (int i = 0; i < n; i++) {
 		 StringTokenizer st = new StringTokenizer(br.readLine());
 		 int x = Integer.parseInt(st.nextToken());
 		 int y = Integer.parseInt(st.nextToken());
 		 
-		 for (int j = x; j <= x+10; j++) {
-			 arr[j][y] = 1;
-			 arr[j][y+10] = 1;
-		}
-		 
-		 for (int j = y; j <= y+10; j++) {
-			arr[j][x] = 1;
-			arr[j][x+10] = 1;
+		 for (int j = x; j < x+10; j++) {
+			for (int j2 = y; j2 < y+10; j2++) {
+				arr[j][j2] = 1;
+			}
 		}
 		 
 	}
+	 
+	int sum = 0;
+	for (int i = 1; i <= 100; i++) {
+		for (int j = 1; j <= 100; j++) {
+			if(arr[i][j]==1) {
+				for (int j2 = 0; j2 < 4; j2++) {
+					if(arr[i+dir[j2][0]][j+dir[j2][1]]==0) sum++;
+				}
+			}
+		}
+	}
+	
+	System.out.println(sum);
 	 
 	}
 }
